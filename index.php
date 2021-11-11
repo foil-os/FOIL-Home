1724e8ed59157879ae489e991d6b464218502563
Senal Bulumulle <senal.bulumulle@gmail.com>
2021-11-11 14:07:34 -0500

    Imported the mockup version of the FOIL-Home Software
    This branch, "FOIL-Home-Mockup-UI" only contains the mockup only, user interface version of the FOIL-Home Software. Throughout the releases of the FOIL-Home Software, this branch will be used for mockup UI design, as well as for UI Testing before merging into the main branch.

--- index.php
+++ index.php
@@ -1,16 +1,24 @@
 <html>
-   <head>
-      <title>FOIL Home</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link href="/System_Files/css/FOIL-Bar.css" rel="stylesheet">
-      <script src="/System_Files/js/Auto_Display.js"></script>
-      <noscript>
-         <link href="/System_Files/css/index.css" rel="stylesheet">
-      </noscript>
-   </head>
+<head>
+<title>FOIL Home</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="/System_Files/css/FOIL-Bar.css" rel="stylesheet">
+<link href="/System_Files/css/FOIL-UI.css" rel="stylesheet">
+<link href="/System_Files/css/index.css" rel="stylesheet">
+</head>
    
    <body>
-      <div class="home-screen-main-screen color">
+      <div class="FOIL-Navigation-Bar color-is-blue">
+         <FOIL-Font-Size-19></FOIL-Font-Size-19>
+         <FOIL-Font-Size-13><script src="Greeting.js"></script></FOIL-Font-Size-13>
+         <a href="../../APPS/Calculator/index.php" class="FOIL-button-small">Calculator</a>
+         <a href="../../APPS/AnalogClock/index.php" class="FOIL-button-small">Analog Clock</a>
+         <a href="../../APPS/Settings/index.php" class="FOIL-button-small">Settings</a>
+         <button class="FOIL-button-small color-is-red"><FOIL-Font-Size-31>&#9212; </FOIL-Font-Size-31><FOIL-Font-Size-13>Shutdown</FOIL-Font-Size-14></button>
+         <button class="FOIL-button-small color-is-red"><FOIL-Font-Size-31>&#9212;</FOIL-Font-Size-31> <FOIL-Font-Size-13>Restart</FOIL-Font-Size-14></button>
+      </div>
+         
+      <div class="home-screen-main-screen">
          <div class="window-blinds-effect">
             <h1>
                <div id="time_service"></div>
@@ -21,11 +29,6 @@
             <script src="/System_Files/js/FOIL-Time.js"></script>
          </div>
       </div>
-      </div>
-      
-      <div class="home-screen-secondary-screen color">
-            <a href="#Applications" class="button">Applications</a>
-            <?php include("System_Files/php/system-applications-menu.php"); ?>
-      </div>
+     </div>
    </body>
 </html>
