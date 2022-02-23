82fa3ce92097c20bfec322cbc33372c6f06b21ad
senalbulumulle <senal.bulumulle@gmail.com>
2022-02-23 13:59:10 -0500

    Created Menu for Lesson User Interface

---
+++ index.php
@@ -0,0 +1,77 @@
+<html>
+
+
+<head>
+
+
+        <title>FOIL Home</title>
+
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+        <link href="/System_Files/css/FOIL-Bar.css" rel="stylesheet">
+
+
+        <link href="/System_Files/css/FOIL-UI.css" rel="stylesheet">
+
+
+        <link href="/System_Files/css/index.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+        <!-- 
+        This is where the buttons take place
+        For example, Apps like Calculator, Analog Clock, and Settings are placed on the FOIL-UI Navigation bar for more 
+        easier access, instead of eariler software versions of FOIL-Home. 
+      
+        -->
+
+
+        <div class="FOIL-Navigation-Bar color-is-blue">
+                <FOIL-Font-Size-13>
+                        <script src="Greeting.js"></script>
+                </FOIL-Font-Size-13>
+
+                <FOIL-Font-Size-24>
+                        <div id="time_service"></div>
+                </FOIL-Font-Size-24>
+
+                <h2>Lesson</h2>
+                <script src="/System_Files/js/FOIL-Time.js"></script>
+
+                <!-- APPS -->
+
+                <a href="#" class="FOIL-button-small color-is-red">Back</a>
+                <a href="#" class="FOIL-button-small color-is-blue">Analog Clock</a>
+                <a href="#" class="FOIL-button-small color-is-blue">Settings</a>
+
+        </div>
+
+
+
+
+        <div class="FOIL-Layout color-is-blue">
+                <h1>
+                        <FOIL-Font-Size-54>
+                                Lesson
+                        </FOIL-Font-Size-54>
+                </h1>
+
+                <!-- Buttons -->
+
+                <button class="FOIL-button color-is-blue">Button 1</button>
+                <button class="FOIL-button color-is-blue">Button 2</button>
+                <button class="FOIL-button color-is-blue">Button 3</button>
+                <button class="FOIL-button color-is-blue">Button 4</button>
+                <button class="FOIL-button color-is-blue">Button 5</button>
+        </div>
+
+
+</body>
+
+
+</html>
