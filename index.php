9847d190d75f70057d9fd652de5ece8618cfe8c2
senalbulumulle <senal.bulumulle@gmail.com>
2022-02-23 14:41:00 -0500

    Apps menu prototype

---
+++ index.php
@@ -0,0 +1,80 @@
+<html>
+
+
+<head>
+
+
+        <title>APPS Menu</title>
+
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+        <link href="/System_Files/css/FOIL-Bar.css" rel="stylesheet">
+
+
+        <link href="/System_Files/css/FOIL-UI.css" rel="stylesheet">
+
+
+        <link href="/System_Files/css/index.css" rel="stylesheet">
+
+
+        <link href="config.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+        <!-- 
+        This is where the buttons take place
+        For example, Apps like Calculator, Analog Clock, and Settings are placed on the FOIL-UI Navigation bar for more 
+        easier access, instead of eariler software versions of FOIL-Home. 
+      
+        -->
+
+
+        <div class="FOIL-Navigation-Bar">
+                <FOIL-Font-Size-13>
+                        <script src="Greeting.js"></script>
+                </FOIL-Font-Size-13>
+
+                <FOIL-Font-Size-24>
+                        <div id="time_service"></div>
+                </FOIL-Font-Size-24>
+
+                <h2>APPS</h2>
+                <script src="/System_Files/js/FOIL-Time.js"></script>
+
+                <!-- APPS -->
+
+                <a href="#" class="FOIL-button-small color-is-black">Back</a>
+                <a href="#" class="FOIL-button-small color-is-black">Analog Clock</a>
+                <a href="#" class="FOIL-button-small color-is-black">Settings</a>
+
+        </div>
+
+
+
+
+        <div class="FOIL-Layout">
+                <h1>
+                        <FOIL-Font-Size-54>
+                                Apps
+                        </FOIL-Font-Size-54>
+                </h1>
+
+                <!-- Buttons -->
+
+                <button class="FOIL-button color-is-black">APP 1</button>
+                <button class="FOIL-button color-is-black">APP 2</button>
+                <button class="FOIL-button color-is-black">APP 3</button>
+                <button class="FOIL-button color-is-black">APP 4</button>
+                <button class="FOIL-button color-is-black">APP 5</button>
+        </div>
+
+
+</body>
+
+
+</html>
