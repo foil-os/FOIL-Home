426dc36fd427f64cc8ca869e84b6f587aeb120bc
senalbulumulle <senal.bulumulle@gmail.com>
2022-03-17 09:54:31 -0400

    Added new Settings App Menu

---
+++ APPS/Settings/index.php
@@ -0,0 +1,60 @@
+<html>
+
+
+<head>
+    <title>Settings</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="/System_Files/css/FOIL-Bar.css" rel="stylesheet">
+    <link href="/System_Files/css/FOIL-UI.css" rel="stylesheet">
+    <link href="/System_Files/css/index.css" rel="stylesheet">
+    <link href="config.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <!-- 
+        This is where the buttons take place
+        For example, Apps like Calculator, Analog Clock, and Settings are placed on the FOIL-UI Navigation bar for more 
+        easier access, instead of eariler software versions of FOIL-Home. 
+      
+        -->
+
+
+    <div class="FOIL-Navigation-Bar">
+        <FOIL-Font-Size-13>
+            <script src="Greeting.js"></script>
+        </FOIL-Font-Size-13>
+
+        <FOIL-Font-Size-24>
+            <div id="time_service"></div>
+        </FOIL-Font-Size-24>
+
+        <h2>APPS</h2>
+        <script src="/System_Files/js/FOIL-Time.js"></script>
+
+        <!-- APPS -->
+
+        <a href="../../index.php" class="FOIL-button-small">Back</a>
+        <a href="../AnalogClock/index.php" class="FOIL-button-small">Analog Clock</a>
+        <a href="../Settings/index.php" class="FOIL-button-small">Settings</a>
+
+    </div>
+
+
+
+
+    <div class="FOIL-Layout">
+        <h1>
+            <FOIL-Font-Size-54>
+                Settings                        <!-- Settings app -->
+            </FOIL-Font-Size-54>
+        </h1>
+
+
+    </div>
+
+
+</body>
+
+
+</html>
